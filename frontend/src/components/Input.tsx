cec771f62fdced27b876c7375407d11f23c55ca4
Alexander Volle <alexander.volle@hotmail.com>
2025-03-22 20:43:41 +0100

    Allow searching with empty string, which would return all results

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -10,5 +10,5 @@ export default function Input(props: InputProps) {
         (props.className ?? "") +
         " block w-full rounded-md border-0 py-1.5 theme-input shadow-sm ring-1 ring-inset ring-gray-300 dark:ring-gray-50 placeholder:text-gray-400 placeholder:dark:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6";
 
-    return <input id={id} className={className} type="text" autoComplete="off" required {...props} />;
+    return <input id={id} className={className} type="text" autoComplete="off" {...props} />;
 }
